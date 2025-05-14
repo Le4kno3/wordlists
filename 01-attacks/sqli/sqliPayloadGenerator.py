c6f2f30b965fa89b72d3c5e64df24e8f51e7183e
le4kno3 <le4kno3@gmail.com>
2025-05-14 21:39:12 +0530

    updates

--- 01-attacks/sqli/sqliPayloadGenerator.py
+++ 01-attacks/sqli/sqliPayloadGenerator.py
@@ -1,58 +1,89 @@
 import re
 
 if __name__ == '__main__':
-    PAYLOAD_HOME = '/home/playbox/Documents/Github/wordlists/01-attacks/sqli/'
-    template_file = PAYLOAD_HOME + 'template_and_helpers/TEMPLATE.txt'
-    helper_input_complete = PAYLOAD_HOME + 'template_and_helpers/helper_input_complete.txt'
-    helper_force_end = PAYLOAD_HOME + 'template_and_helpers/helper_force_end.txt'
-    helper_values = PAYLOAD_HOME + 'template_and_helpers/helper_values.txt'
-    js_payloads = []
-    new_js_payloads = []
-    value_payloads = []
-    new_xss_payloads = []
+    ATTACK_HOME = '/home/playbox/Documents/Github/wordlists/01-attacks/sqli/'
+    template_file_path = ATTACK_HOME + 'templates_and_helpers/TEMPLATE.txt'
+    helper_input_complete_path = ATTACK_HOME + 'templates_and_helpers/helper_input_complete.txt'
+    helper_input_complete = []
+    helper_force_end_path = ATTACK_HOME + 'templates_and_helpers/helper_force_end.txt'
+    helper_force_end = []
+    helper_values_path = ATTACK_HOME + 'templates_and_helpers/helper_values.txt'
+    helper_values = []
+    helper_toggle_TF_path = ATTACK_HOME + 'templates_and_helpers/helper_toggle_TF.txt'
+    helper_toggle_TF = []
+    helper_NOSQLi_path = ATTACK_HOME + 'templates_and_helpers/helper_NO_SQLi.txt'
+    helper_NOSQLi = []
+    sqli_final_payload_path = ATTACK_HOME + 'ALL_sqli_payloads'
 
-    with open(value_file_path) as value_file:
-        value_payloads = value_file.read().splitlines()#parse new line delimited array
+    temp1_sqli_payloads = []
+    temp2_sqli_payloads = []
+    temp3_sqli_payloads = []
+    new_sqli_payloads = []
 
-    # print(value_payloads)
+    # Prepare Payload - values
+    with open(helper_values_path) as file_data:
+        helper_values = file_data.read().splitlines()#parse new line delimited array
 
-    with open(javascript_file_path) as js_file:
-        js_payloads = js_file.read().splitlines()#parse new line delimited array
-        for js_payload in js_payloads:
-            for value_payload in value_payloads:
-                temp = js_payload.replace('{PAYLOAD}', value_payload)
-                new_js_payloads.append(temp)
+    # Prepare Payload - toggle true false (TF)
+    with open(helper_toggle_TF_path) as file_data:
+        helper_toggle_TF = file_data.read().splitlines()#parse new line delimited array
 
-    # print(new_js_payloads)
+    # Prepare Payload - force end
+    with open(helper_force_end_path) as file_data:
+        helper_force_end = file_data.read().splitlines()#parse new line delimited array
 
-    with open(xss_file_path) as xss_file:
-        xss_payloads = xss_file.read().splitlines()#parse new line delimited array
-        for xss_payload in xss_payloads:
-            for js_payload in new_js_payloads:
-                temp = xss_payload.replace('{JAVASCRIPT}', js_payload)
-                if('{PAYLOAD}' in temp):
-                    for value_payload in value_payloads:
-                        temp = xss_payload.replace('{PAYLOAD}', value_payload)
-                        new_xss_payloads.append(temp)
-                else:
-                    new_xss_payloads.append(temp)
+    # Prepare Payload - input complete
+    with open(helper_input_complete_path) as file_data:
+        payloads = file_data.read().splitlines()#parse new line delimited array
+        for payload in payloads:
+            for value_payload in helper_values:
+                temp = payload.replace('{VALUE}', value_payload)
+                helper_input_complete.append(temp)
 
+    # Prepare Payload - NO SQLi
+    with open(helper_NOSQLi_path) as file_data:
+        helper_NOSQLi = file_data.read().splitlines()#parse new line delimited array
 
-    # print(new_xss_payloads)
+    # Generate payloads now
+    with open(template_file_path) as template_file:
+        sqli_payloads = template_file.read().splitlines()#parse new line delimited array
+        for sqli_payload in sqli_payloads:
+            # resolve input complete
+            if('{INPUT_COMPLETE}' in sqli_payload):
+                for input_complete in helper_input_complete:
+                    temp1 = sqli_payload.replace('{INPUT_COMPLETE}', input_complete)
+                    temp1_sqli_payloads.append(temp1)
+        
+        for sqli_payload in temp1_sqli_payloads:
+            # resolve force end
+            if('{FORCE_END}' in sqli_payload):
+                for force_end in helper_force_end:
+                    temp1 = sqli_payload.replace('{FORCE_END}', force_end)
+                    temp2_sqli_payloads.append(temp1)
 
-    # handle values in the final javascript file also
+        for sqli_payload in temp2_sqli_payloads:
+            # resolve toggle TF
+            if('{TOGGLE_TF}' in sqli_payload):
+                for toggle_TF in helper_toggle_TF:
+                    temp2 = sqli_payload.replace('{TOGGLE_TF}', toggle_TF)
+                    temp3_sqli_payloads.append(temp2)
 
-    with open(xss_final_payload_path, "w") as xss_final_file:
-        for xss_payload in new_xss_payloads:
-            xss_final_file.writelines(f"{xss_payload}\n")
+        # We dont need to resovle values, as it is already resolved by Input Complete but still if {VALUE} in final template then
+        for sqli_payload in temp3_sqli_payloads:
+            # resolve toggle TF
+            if('{VALUE}' in sqli_payload):
+                for value in helper_values:
+                    temp3 = sqli_payload.replace('{VALUE}', value)
+                    new_sqli_payloads.append(temp3)
+            else:
+                new_sqli_payloads.append(sqli_payload)
+        
+        # Append all NO SQLi payloads, currently the NO SQLi payloads are not customizable. 
+        for nosqli_payload in helper_NOSQLi:
+            new_sqli_payloads.append(nosqli_payload)
+            
 
-    # with open(template_file_path) as file:
-    #
-    #     while line := file.readline():
-    #         print(line.rstrip())
-    #         line.replace('{JAVASCRIPT}', )
-    #
-    #
-    #     parse this string, and split by new line
-    #     for line in template_file_data:
-    #         print(line.rstrip())
+    # Finally Write lines in output file which will have the list of all sqli final payloads
+    with open(sqli_final_payload_path, "w") as sqli_final_file:
+        for sqli_payload in new_sqli_payloads:
+            sqli_final_file.writelines(f"{sqli_payload}\n")
